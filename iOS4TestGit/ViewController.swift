86a3bdf6640679fe813b4078f2bc3643ec902184
Dmitrii Zorin <zchakiz@gmail.com>
2022-02-13 13:42:37 +0300

    Initial Commit

---
+++ iOS4TestGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS4TestGit
+//
+//  Created by Zorin Dmitrii on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
